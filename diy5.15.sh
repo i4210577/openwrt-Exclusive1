96135e3565caeffa0ed5a61ed58cc0eb525325da
i4210577 <90830668+i4210577@users.noreply.github.com>
2022-06-28 08:54:36 -0700

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -24,6 +24,8 @@ git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 sed -i '$a src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 # echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
@@ -33,6 +35,8 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-store/Makefile
 # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
+# git clone https://github.com/linkease/istore package/istore
+# git clone https://github.com/linkease/istore-ui package/istore-ui
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
