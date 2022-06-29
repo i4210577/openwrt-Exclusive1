fabc8c3279547d3c8773a082d5b1d296fe3417d3
i4210577 <90830668+i4210577@users.noreply.github.com>
2022-06-28 23:28:17 -0700

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -31,16 +31,12 @@ git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-# svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
-# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
-# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
-# sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-store/Makefile
-# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-git clone https://github.com/linkease/istore package/istore
-git clone https://github.com/linkease/istore-ui package/istore-ui
+sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-store/Makefile
+# sed -i 's/luci-lib-ipkg/luci-base/g' package/feeds/kenzok/luci-app-store/Makefile
+git clone https://github.com/linkease/istore package/luci-app-store
+git clone https://github.com/linkease/istore-ui package/app-istore-ui
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 git clone https://github.com/fw876/helloworld.git package/ssr
@@ -64,3 +60,9 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 # git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 # git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 # rm -rf ../lean/luci-theme-argon
+
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+# svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
